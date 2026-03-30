fc06e70bceb535c90f78b8a3775095b7091146ab
Marco-Petronella <marco.petronella85@gmail.com>
2026-03-30 11:44:21 +0200

    snack 2

--- snack-2/script.js
+++ snack-2/script.js
@@ -6,3 +6,16 @@ const people = [
 
 // Stampa in console tutti i nomi
 // Risultato: 'Paolo', 'Giulia', 'Marco'
+
+console.log("stampa con ciclo foreach");
+people.forEach(person => {
+  console.log(person.name);
+})
+
+
+
+const peopleNames = people.map(person => person.name)
+console.log("bonus extra stampa con assegnazione map");
+console.log(peopleNames);
+
+
